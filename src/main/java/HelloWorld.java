0318b311215347a76cbc09fa8ed62f54746e11ec
EthanHunt <1984705234@qq.com>
2019-06-29 22:30:02 +0800

    play

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ethan Hunt
+ * @version 1.0
+ * @date 2019/6/29 22:15
+ * @desc
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello"+"world");
+    }
+}
